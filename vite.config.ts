6f3c1a96c614d345e6fe07b606522313d12c343f
Quentin Widlocher <quentin@widlocher.com>
2023-04-11 18:06:03 +0200

    ⚡️ Add PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,29 @@ export default defineConfig({
 		solidPlugin(),
 		VitePWA({
 			registerType: "autoUpdate",
+			includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+			manifest: {
+				name: "MTG Proxy Maker",
+				short_name: "MTG Proxy Maker",
+				background_color: "#44403c",
+				theme_color: "#f59e0c",
+				scope: "/",
+				orientation: "portrait",
+				start_url: "/",
+				display: "standalone",
+				icons: [
+					{
+						src: "/android-chrome-192x192.png",
+						sizes: "192x192",
+						type: "image/png",
+					},
+					{
+						src: "/android-chrome-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+					},
+				],
+			},
 			workbox: {
 				globPatterns: [
 					"**/*.{js,css,html,ico,png,svg}",
